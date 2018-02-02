6e35db666c0d6ce4121cfa0c0cf7284698673250
Wes Hager <weshager@wess-mbp.attlocal.net>
2018-02-01 23:59:24 -0600

    make it better

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || 'ak3048VKSKVFBBFBS6374HchebrGDJVNcbeh7575647321002938573881jJASOp0w7s7HJn', //Add your master key here. Keep it secret!
+  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'https://pano-amigo.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
