f7cf27c704f315c2a77733d12421e2a5a2e082f4
Diamond Geezer <wes.hager78@yahoo.com>
2018-02-02 03:16:00 -0600

    poop

--- cloud/main.js
+++ cloud/main.js
@@ -3,15 +3,9 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
 
+
 Parse.Push.send({
                 channels: [channel],
                 data: data
                 }, { useMasterKey: true });
 
-Parse.Cloud.afterSave(Parse.User, function(request, response) {
-    if (!request.object.get("email")) {
-        response.error("email is required for signup");
-    } else {
-        response.success();
-    }
-});
